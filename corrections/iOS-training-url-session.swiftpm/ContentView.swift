eed077151eccae832b510695b0a820f1758e7e79
Yassine Benabbas <yassine.benabbas@worldline.com>
2022-11-22 15:51:45 +0100

    Corrigé à jour de url session

--- corrections/iOS-training-url-session.swiftpm/ContentView.swift
+++ corrections/iOS-training-url-session.swiftpm/ContentView.swift
@@ -2,32 +2,39 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var results = [Result]()
-
+    @State private var isLoading = true
+    
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
+        if isLoading {
+            ProgressView().task {
+                await loadData(items: results)
+                try? await Task.sleep(nanoseconds: 1_000_000_000)
+                isLoading = false
             }
-        }.task {
-            await loadData()
+        } else {
+            List(results, id: \.trackId) { item in
+                VStack(alignment: .leading) {
+                    Text(item.trackName)
+                        .font(.headline)
+                    Text(item.collectionName)
+                }
+            }
         }
+    }
+}
+
+func loadData() async -> [Result] {
+    guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+        print("Invalid URL")
+        return []
     }
     
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print("Invalid data")
-        }
+    do {
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let decodedResponse = try JSONDecoder().decode(Response.self, from: data)
+        return decodedResponse.results
+    } catch {
+        print("Invalid data")
     }
+    return []
 }
