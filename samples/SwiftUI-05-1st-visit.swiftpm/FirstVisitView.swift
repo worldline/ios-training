8b023d5a06043215df0ea122bee0446d7a6e2562
yostane <1958676+yostane@users.noreply.github.com>
2025-10-02 16:38:56 +0200

    feat: Add Pokémon list, detail views, and first visit handling

---
+++ samples/SwiftUI-05-1st-visit.swiftpm/FirstVisitView.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct FirstVisitView : View {
+    @Binding var completed: Bool
+    var body : some View {
+        VStack {
+            Text("Welcome to this awesome App")
+            Button {
+                completed = true
+                UserDefaults.standard.set(true, forKey: "FirstVisitCompleted")
+            } label: {
+                Text("Close").border(.primary)
+            }
+        }
+    }
+}
+
+#Preview {
+    @Previewable @State var completed = false
+    FirstVisitView(completed: $completed)
+}
---
+++ samples/SwiftUI-05-1st-visit.swiftpm/PokemonDetailView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct PokemonDetailView : View {
+    let pokemon: Pokemon
+    var body: some View {
+        VStack {
+            Text(pokemon.name)
+            AsyncImage(url: pokemon.imageUrl)
+            Text("Details: \(pokemon.url)")
+        }
+    }
+}
+
+#Preview {
+    PokemonDetailView(pokemon: pokemons.first!)
+}
