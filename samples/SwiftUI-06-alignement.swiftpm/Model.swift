aac38e010a3411dc3770ecb376f09d618380aa8c
yostane <1958676+yostane@users.noreply.github.com>
2025-10-03 11:15:20 +0200

    feat: Add Pokémon navigation and detail views with first visit handling

---
+++ samples/SwiftUI-06-alignement.swiftpm/Model.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+struct Pokemon: Identifiable {
+    var id: String {
+        get { name }
+    }
+    
+    let name: String
+    let url: String
+    var imageUrl: URL? {
+        get {
+            let id = url.trimmingPrefix("/").split(separator: "/").last ?? "0"
+            return URL(string:  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(id).png")
+        }
+    }
+}
+
+let pokemons = [
+    Pokemon(name:"bulbasaur",url:"https://pokeapi.co/api/v2/pokemon/1/"),
+    Pokemon(name:"ivysaur",url:"https://pokeapi.co/api/v2/pokemon/2/")
+]
